13318072ff6ee2437faef53371f31b88709b49f6
lewisgra000 <lewisgra000@stu.oasd.org>
2019-02-15 17:35:08 -0600

    Made basic button for lead screws

---
+++ src/main/java/frc/robot/commands/leadScrewJoystick.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class leadScrewJoystick extends Command {
+  public leadScrewJoystick() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.leadScrews);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+   
+    //boolean leftLift = 
+   // double rightLift = Robot.m_oi.getLeftTriggerAxis(Robot.m_oi.intakeController);
+
+    if (Robot.m_oi.xButton1.get() == true) {
+      Robot.leadScrews.motorSpeed(1);
+    } else{
+      Robot.leadScrews.motorSpeed(0);
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.leadScrews.motorSpeed(0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
