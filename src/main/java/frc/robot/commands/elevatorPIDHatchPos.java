43602d2e6bbcf23a819ce0b5b2bd6460615171bd
Rivest8 <7680684+Rivest8@users.noreply.github.com>
2019-03-07 23:52:58 -0600

    TODO: will the code actualy be ever interpted if the joystics are moved???

--- src/main/java/frc/robot/commands/elevatorPIDHatchPos.java
+++ src/main/java/frc/robot/commands/elevatorPIDHatchPos.java
@@ -118,7 +118,7 @@ public class elevatorPIDHatchPos extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return false; 
   }
 
   // Called once after isFinished returns true
@@ -131,6 +131,6 @@ public class elevatorPIDHatchPos extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    end();
+    end();//TODO: will the code actualy be ever interpted if the joystics are moved???
   }
 }
